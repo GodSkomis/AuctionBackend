c51371d91280b388b068c4fb5aa3e1884aa06583
GodSkomis <iiaxa1213@gmail.com>
2023-08-11 03:23:16 +0300

    redis hostname fix

--- auc/celery.py
+++ auc/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auc.settings')
-app = Celery('auc', backend="rpc://", broker="redis://auction-celery:6379/0")
+app = Celery('auc', backend="rpc://", broker="redis://auction-redis:6379/0")
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
