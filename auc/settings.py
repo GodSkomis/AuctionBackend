fea41354dad759433b57854a96d5222d4fd8e783
GodSkomis <iiaxa1213@gmail.com>
2023-08-11 19:34:56 +0200

    db path fix

--- auc/settings.py
+++ auc/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'auc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f"{BASE_DIR}/data/users.sqlite3",
+        'NAME': os.path.join(BASE_DIR, "/data/users.sqlite3")
     }
 }
 
